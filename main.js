5ad243095834c59450310ecdc9433c2da6e94608
razan <almusarazan@gmail.com>
2018-10-14 09:19:10 +0300

    project1

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready ((function){
+
+//     let player1 = "X";
+//     let player2 = "O";
+//     let currentTurn = 1;
+//     let move = 0;
+
+//  let sqr = $(".colum");
+
+// sqr.on("click", function (
+// )
+
+// )
+
+})
